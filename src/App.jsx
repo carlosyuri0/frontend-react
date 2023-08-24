f8ece9cc05991b64fb05bcccf7409b520d0101ed
Carlos Yuri <carlosyurialvesbenedito@gmail.com>
2023-08-24 15:07:08 -0300

    separação layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import Header from "./Header"
-import Sidebar from "./Sidebar"
-import Content from "./Content"
-import Footer from "./Footer"
+import Home from "./components/pages/Home"
 
 import "./App.css"
 
 const App = () => {
   return (
     <>
-       <Header />
-       <div id="main">
-          <Sidebar />
-          <Content />
-       </div>
-       <Footer />
+    <Home />
     </>
   )
 }
