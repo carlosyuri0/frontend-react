c88e4aaf325098f871e2e00f2ca5528fea5f1562
Carlos Yuri <carlosyurialvesbenedito@gmail.com>
2023-08-24 15:58:28 -0300

    introduction routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Home from "./components/pages/Home"
-
+import QuemSomos from "./components/pages/QuemSomos"
+import Page404 from "./components/pages/Page404"
+import {Routes, Route} from "react-router-dom"
 import "./App.css"
 
 const App = () => {
   return (
-    <>
-    <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/quem-somos" element={<QuemSomos />} />
+      <Route path="*" element={<Page404 />} />
+
+    </Routes>
   )
 }
 
